89ce271af97ab3c6e497c0fc167eff1b76d69fc7
Tuğrul Bayrak <tugrulbyrk1@gmail.com>
2017-08-12 15:50:08 +0300

    android-arduino iletişim başarılı

--- app/src/main/java/com/smartcontrolsystem/inteliworld/MainActivity.java
+++ app/src/main/java/com/smartcontrolsystem/inteliworld/MainActivity.java
@@ -2,12 +2,47 @@ package com.smartcontrolsystem.inteliworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.client.Firebase;
+import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button num1;
+    private Firebase mRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        Firebase.setAndroidContext(this);
+        mRef = new Firebase("https://inteliworld-31e3b.firebaseio.com/");
+
+
+
+
+        num1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Firebase mRefChild = mRef.child("num1");
+                mRefChild.setValue("false");
+
+            }
+        });
+
+
+
     }
+
+
+    public void init() {
+        num1 = (Button)findViewById(R.id.num1);
+    }
+
+
+
 }
